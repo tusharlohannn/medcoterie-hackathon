98abb25044637be08057b30a9edfb6746f7411c0
tusharlohannn <tusharlohan2222@gmail.com>
2025-08-17 12:42:44 +0530

    ssl

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // important for Render
+  },
 });
 
 module.exports = pool;
